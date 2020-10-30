092848dee828b13d60213e13cdf2729849683eaa
yukairui <825294867@qq.com>
2020-10-30 18:23:14 +0800

    改

--- src/layout/Content/index.js
+++ src/layout/Content/index.js
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 const basic = React.lazy(() => import('views/report/basic'))
 const personas = React.lazy(() => import('views/report/personas'))
@@ -19,9 +19,6 @@ export default class View extends React.Component {
   render() {
     return (
       <>
-        <Link to="/">1</Link>
-        <Link to="/report/interaction">2</Link>
-        <Link to="/report/personas">3</Link>
         <Suspense fallback={ <div>Loading...</div> }>
           <Route path="/report/interaction" exact component={basic}></Route>
           <Route path="/report/personas" component={personas}></Route>
