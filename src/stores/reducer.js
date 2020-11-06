027266880e6264861cfd56ed7b6de2aa30d13fb3
yukairui <825294867@qq.com>
2020-11-06 17:51:57 +0800

    连接redux跟组件

--- src/stores/reducer.js
+++ src/stores/reducer.js
@@ -1,4 +1,4 @@
-import { combineReducer, combineReducers } from 'redux'
+import { combineReducers } from 'redux'
 import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions'
 
 const  { SHOW_ALL } = VisibilityFilters
